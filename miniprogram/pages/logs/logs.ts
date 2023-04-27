1568ec6e5845d353a49ca15d51a18f117999d462
fukai03 <fukai03@baidu.com>
2023-04-27 16:35:24 +0800

    feat: project init

---
+++ miniprogram/pages/logs/logs.ts
@@ -0,0 +1,25 @@
+// logs.ts
+// const util = require('../../utils/util.js')
+import { formatTime } from '../../utils/util'
+
+Page({
+  data: {
+    logs: [],
+  },
+  onLoad() {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map((log: string) => {
+        return {
+          date: formatTime(new Date(log)),
+          timeStamp: log
+        }
+      }),
+    })
+  },
+  clearLogs() {
+    wx.removeStorageSync('logs')
+    this.setData({
+      logs: []
+    })
+  }
+})
